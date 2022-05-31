ad1152e638a2a32cfa10848e7d24034d94ba833f
Jiannan Yang <82540281+longitude-jyang@users.noreply.github.com>
2022-05-31 21:29:45 +0100

    cal_h update with waitbar and dummy function

--- code/cal_h.m
+++ code/cal_h.m
@@ -5,34 +5,46 @@
 
 
 % 22/04/2021 @ Franklin Court, Cambridge  [J Yang] --> initial trial 
+% 32/05/2022 @ Franklin Court, Cambridge  [J Yang] --> update with dummy
+% function and waitbar
 
 function h_Results = cal_h (xS, Opts)
 
     % assign options
     nS = Opts.nSampMC;
     hfunction = str2func(Opts.funName);
-    
-    % do a run to check size of output y
-    y0 = hfunction(xS.samp(1,:));
-    Ne = length(y0);
-    
-    % loop over all samples 
-    y = zeros(nS,Ne);
-    
-    waith = waitbar(0,'Please wait...');
-    
-    for ii=1:nS
+
+    waith = waitbar(0,'Please wait...',...
+        'CreateCancelBtn','setappdata(gcbf,''cancelling'',1)');
+    setappdata(waith,'cancelling',0);
+
+    if strcmp(Opts.funName,'trial')                  % trail function 
+        y = sum(xS.samp);
+    elseif strcmp(Opts.funName,'design_dummy')       % dummy function 
+        y = xS.samp;
+
+    else
+        % do a run to check size of output y
+        y0 = hfunction(xS.samp(1,:));
+        Ne = length(y0);
         
-        if strcmp(hfunction,'trail')                    % dummy function 
-            y (ii,:)= sum(xS.samp(ii,:));
+        % loop over all samples 
+        y = zeros(nS,Ne);
+        
+        waith = waitbar(0,'Please wait...');
+        
+        for ii=1:nS
+            if getappdata(waith,'cancelling')
+                    break
+            end
+            
+            y(ii,:) = hfunction(xS.samp(ii,:),Opts);
+            
+            waitbar(ii / nS, waith, strcat(num2str(ii),'/',num2str(nS),'{ }','Samples' )); 
         end
-        
-        y(ii,:) = hfunction(xS.samp(ii,:));
-        
-        waitbar(ii / nS); 
     
     end
     
-    close(waith)
+    delete(waith)
     
     h_Results.y = y;
